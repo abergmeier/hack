e7c47556797feefc1df43dd3156f3707a8c5829e
LCID Fire <lcid-fire@gmx.net>
2012-06-26 15:11:00 +0200

    Add States class

---
+++ src/logic/states.cpp
@@ -0,0 +1,50 @@
+/*
+ * states.cpp
+ *
+ *  Created on: Jun 23, 2012
+ *      Author: andreas
+ */
+
+#include "states.hpp"
+
+using namespace hack::state;
+
+States States::INSTANCE;
+
+States& States::Get() {
+	return INSTANCE;
+}
+
+void States::Commit( const Serializable& object ) {
+	std::stringstream stream;
+	object.Serialize( stream );
+	auto& data = stream.str();
+
+	if( _network )
+		PassToNetwork( data );
+	else
+		_queue.push_back( data );
+}
+
+//void CommitTo( const Serializable& object, Player& player);
+
+void States::PassToNetwork( const std::string& data ) {
+	_network->Send( data );
+}
+
+void States::SetNetwork( hack::net::Network& network ) {
+	_network = &network;
+
+	if( !_network )
+		return;
+
+	// We have a network connection
+	for( auto& str : _queue ) {
+		PassToNetwork( str );
+	}
+
+	// We passed everything to network so forget
+	_queue.clear();
+}
+
+
---
+++ src/logic/states.hpp
@@ -0,0 +1,39 @@
+/*
+ * states.hpp
+ *
+ *  Created on: Jun 12, 2012
+ *      Author: andreas
+ */
+
+#ifndef STATES_HPP_
+#define STATES_HPP_
+
+#include <map>
+#include <memory>
+#include <deque>
+#include <string>
+#include "../net/network.hpp"
+#include "serializable.hpp"
+#include "player.hpp"
+
+namespace hack {
+namespace state {
+
+class States {
+	static States INSTANCE;
+	hack::net::Network* _network = nullptr;
+	// If no network is connected save data locally
+	std::deque<std::string> _queue;
+	void PassToNetwork( const std::string& data );
+public:
+	static States& Get();
+	void Commit( const Serializable& object );
+	void CommitTo( const Serializable& object, Player& player);
+	//std::map< std::weak_ptr<void>, std::unique_ptr<void> > _state;
+	void SetNetwork( hack::net::Network& network );
+};
+
+} } //namespace hack
+
+
+#endif /* STATES_HPP_ */
