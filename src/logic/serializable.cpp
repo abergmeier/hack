0b220e48c200b93f0cdf82903f0e83a050a8f58f
LCID Fire <lcid-fire@gmx.net>
2012-06-26 13:32:01 +0200

    Introduce Serializable class for serializing support.

---
+++ src/logic/serializable.cpp
@@ -0,0 +1,20 @@
+/*
+ * serializable.cpp
+ *
+ *  Created on: Jun 25, 2012
+ *      Author: andreas
+ */
+
+#include "serializable.hpp"
+
+using namespace hack::state;
+
+void Serializable::Serialize( std::ostream& stream ) const {
+	stream << '{' << SerializeContent(stream) << '}';
+}
+
+std::ostream& Serializable::SerializeContent(const std::string& className, std::ostream& stream) const {
+	return stream << className;
+}
+
+
---
+++ src/logic/serializable.hpp
@@ -0,0 +1,33 @@
+/*
+ * serializable.hpp
+ *
+ *  Created on: Jun 25, 2012
+ *      Author: andreas
+ */
+
+#ifndef SERIALIZABLE_HPP_
+#define SERIALIZABLE_HPP_
+
+#include <string>
+#include <ostream>
+
+namespace hack {
+namespace state {
+
+class Serializable {
+public:
+	virtual ~Serializable();
+	void Serialize(std::ostream& stream) const;
+protected:
+	Serializable() = default;
+	// This has to be implemented in order to easily serialize an object
+	// Most times should just call the SerializeContent overload and pass the non abstract class name
+	virtual std::ostream& SerializeContent(std::ostream& stream) const = 0;
+	virtual std::ostream& SerializeContent(const std::string& className, std::ostream& stream) const;
+};
+
+
+} }
+
+
+#endif /* SERIALIZABLE_HPP_ */
