2ccc95cf39225ca31ea76dbaddfab447cc9f3c90
LCID Fire <lcid-fire@gmx.net>
2012-06-26 15:19:29 +0200

    Add Network class

---
+++ src/net/network.cpp
@@ -0,0 +1,413 @@
+
+#include <vector>
+#include <string>
+#include <Poco/Net/MulticastSocket.h>
+#include <chrono>
+#include <exception>
+#include "../logic/session.hpp"
+#include <iostream>
+#include <sstream>
+
+#include "network.hpp"
+
+using namespace hack::net;
+
+namespace {
+	bool DEBUG = true;
+
+	//template <typename... ARGS>
+	void LogState(std::string args) {
+		if( !DEBUG )
+			return;
+
+		std::cout << args;
+		std::cout << std::endl;
+	}
+}
+
+bool operator<(const ENetAddress& lhs, const ENetAddress& rhs) {
+	if( lhs.host == rhs.host )
+		return lhs.port < rhs.port;
+
+	return lhs.host < rhs.host;
+}
+
+class OtherEndpoint {
+
+};
+
+class Peer {
+
+};
+
+namespace {
+	ENetHost* createServer( enet_uint16 incomingPort ) {
+		// Create Server
+		ENetAddress address;
+
+	    address.host = ENET_HOST_ANY;
+	    address.port = incomingPort;
+		auto server = enet_host_create( &address, // create a server host
+		                                32, // allowed 32 clients
+					                    2, // allow up 2 channels to be used, 0 and 1
+					                    0, // any downstream bandwidth
+					                    0  // any upstream bandwidth
+		);
+
+		if( server == nullptr )
+			throw std::runtime_error("An error occurred while trying to create an ENet server.");
+
+		return server;
+	}
+
+	ENetHost* createClient() {
+		auto client = enet_host_create( nullptr, // create a client host
+			                            31, // allowed outgoing connections
+			                            2, // allow up 2 channels to be used, 0 and 1
+			                            0, // any downstream bandwidth
+			                            0  // any upstream bandwidth
+		);
+
+		if( client == nullptr )
+			throw std::runtime_error("An error occurred while trying to create an ENet client.");
+
+		return client;
+	}
+}
+
+Network::Network( enet_uint16 incomingPort )
+{
+	_queues[0] = std::unique_ptr<std::deque<queue_element_type>>(new std::deque<queue_element_type>());
+	_queues[1] = std::unique_ptr<std::deque<queue_element_type>>(new std::deque<queue_element_type>());
+
+	_atomicQueues[0] = _queues[0].get();
+	_atomicQueues[1] = _queues[1].get();
+
+	if( enet_initialize() != 0 )
+		throw std::runtime_error("Could not initialize ENET.");
+
+	try {
+		_server = createServer( incomingPort );
+		_client = createClient( );
+	} catch( ... ) {
+		auto eptr = std::current_exception();
+		// Invoke cleanup
+		Destroy();
+		std::rethrow_exception(eptr);
+	}
+}
+
+void Network::Destroy() {
+	_client = nullptr;
+	_server = nullptr;
+	enet_deinitialize();
+}
+
+Network::~Network() noexcept {
+	Destroy();
+}
+
+void Network::Setup() {
+#if 0
+	STUN_Request stunTask;
+	RegistrationRequest registryTask;
+	
+	registryTask.get();
+	stunTask.get();
+#endif
+}
+
+void Network::Peer::Send(buffer_type buffer, std::function<void()> callback) {
+	auto packet = _createPacket(buffer);
+
+	enet_peer_send(enetPeer, 0, packet);
+	std::async(std::launch::async, callback);
+}
+
+void Network::SetConnectCallback(std::function<void(std::shared_ptr<hack::net::Network::Peer>)> callback) {
+	_connectCallback = callback;
+}
+
+void Network::SetDisconnectCallback(std::function<void(hack::net::Network::Peer&)> callback) {
+	_disconnectCallback = callback;
+}
+
+#if 0
+void Network::Receive() {
+	Poco::Net::SocketAddress address("other", 514);
+	Poco::Net::MulticastSocket socket(Poco::Net::IPAddress(), address.port());
+
+	// to receive any data you must join
+	socket.joinGroup(address.host());
+
+	char buffer[1024];
+
+	for (;;)
+	{
+		Poco::Net::SocketAddress sender;
+		int n = socket.receiveFrom(buffer, sizeof(buffer)-1, sender);
+		buffer[n] = '\0';
+		std::cout << sender.toString() << ": " << buffer << std::endl;
+	}
+}
+
+const std::vector<Session>& Network::GetSessions() const {
+
+}
+#endif
+
+Network::Peer::Peer(ENetAddress address) :
+	address(std::forward<ENetAddress>(address))
+{
+}
+
+Network::Peer::~Peer() {
+
+	if( enetPeer == nullptr )
+		return; //already cleaned up
+
+	enet_peer_reset( enetPeer );
+}
+
+bool Network::Peer::operator <(const Peer& other) const {
+	if( address.host < other.address.host )
+		return true;
+	else if( address.host == other.address.host )
+		return address.port < other.address.port;
+
+	return false;
+}
+
+bool Network::Peer::operator ==(const Peer& other) const {
+	return GetUUID() == other.GetUUID();
+}
+
+void Network::Peer::Disconnect() {
+	enet_peer_disconnect( enetPeer, 0 );
+}
+
+void Network::Peer::Receive(const ENetPacket& packet) {
+	buffer_type data;
+	data.reserve(packet.dataLength);
+	data.assign(packet.data, packet.data + packet.dataLength);
+	receiveCallback(data);
+}
+
+const std::string& Network::Peer::GetUUID() const {
+	return _uuid;
+}
+
+void Network::Peer::SetUUID(std::string uuid) {
+	_uuid = uuid;
+}
+
+void Network::CreatePeer( ENetPeer& peer ) {
+	// We have to build Peer manually on heap here because we only allow Network to
+	// instantiate Peer objects
+	auto sharedPeer = std::shared_ptr<Peer>( new Peer( peer.address ) );
+
+	auto insertPair = _peers.connected.insert( std::make_pair( peer.address, sharedPeer ) );
+
+	if( insertPair.second )
+		return; // Already connected?
+
+	_peers.unconnected.erase( peer.address);
+
+	peer.data = &(*insertPair.first);
+	_connectCallback( sharedPeer );
+}
+
+std::ostream& Network::DEBUG::LOG( std::ostream& stream ) {
+	return std::cout << "[Network] " << stream.rdbuf();
+}
+
+std::ostream& Network::DEBUG::LOG( const std::string& str ) {
+	return std::cout << "[Network] " << str;
+}
+
+std::ostream& Network::DEBUG::ERR( std::ostream& stream ) {
+	return std::cerr << "[Network] " << stream.rdbuf();
+}
+
+void Network::DEBUG::LOG_ENTRY( std::ostream& stream ) {
+	 LOG( stream ) << std::endl;
+}
+
+void Network::DEBUG::LOG_ENTRY( const std::string& str ) {
+	 LOG( str ) << std::endl;
+}
+
+void Network::DEBUG::ERR_ENTRY( std::ostream& stream ) {
+	 ERR( stream ) << std::endl;
+}
+
+void Network::HandleUnconnected() {
+
+	//
+	// Do connection handling
+	//
+	typedef decltype(_peers.unconnected) connections_type;
+
+	connections_type outstandingConnections;
+	{
+		// Lock outstanding for use
+		std::lock_guard<std::mutex> lock( _peers.unconnectedLock );
+
+		outstandingConnections.swap( _peers.unconnected );
+	}
+
+	ENetEvent event;
+	for( auto& entry : outstandingConnections ) {
+
+		DEBUG::LOG_ENTRY(std::stringstream() << "Connecting to " << entry.host << ':' << entry.port);
+		// Initiate the connection, allocating the two channels 0 and 1. */
+		auto peer = enet_host_connect( _client, &entry, 2, 0);
+
+		if (peer == nullptr) {
+		   std::cerr << "CONNECT FAILED" << std::endl;
+		   exit (-1);
+		}
+
+		// Wait 5 seconds for the connection attempt to succeed.
+		if( enet_host_service( _client, &event, 5000) > 0
+		 && event.type == ENET_EVENT_TYPE_CONNECT ) {
+			DEBUG::LOG_ENTRY(std::stringstream() << "Connecting to " << entry.host << ':' << entry.port << " !SUCCEEDED!");
+			CreatePeer( *peer );
+		}
+		else
+		{
+			/* Either the 5 seconds are up or a disconnect event was */
+			/* received. Reset the peer in the event the 5 seconds   */
+			/* had run out without any significant event.            */
+			enet_peer_reset( peer );
+			DEBUG::ERR_ENTRY(std::stringstream() << "Connecting to " << entry.host << ':' << entry.port << " !FAILED!");
+		}
+	}
+}
+
+bool Network::_ExecuteWorker() {
+
+	ENetEvent event;
+
+	HandleUnconnected();
+
+	//
+	// Do input handling
+	//
+
+	auto extractPeer = [&event]() -> std::shared_ptr<Peer> {
+		// We save an address to shared_ptr inside event
+		// Cast it to its original format
+		auto sharedPtr = static_cast<std::shared_ptr<Peer>*>(event.peer->data);
+		// Using shared_ptr is safest when copying - do that
+		auto copyOfSharedPtr = *sharedPtr;
+		// Pass out our copy
+		return std::move(copyOfSharedPtr);
+	};
+
+	// Wait up to 5 milliseconds for an event.
+	if( enet_host_service( _server, &event, 5 ) > 0 ) {
+		switch( event.type ) {
+			case ENET_EVENT_TYPE_NONE:
+				break;
+			case ENET_EVENT_TYPE_CONNECT: {
+				DEBUG::LOG_ENTRY( std::stringstream() << "Peer connected from "
+				                 << event.peer->address.host << ':'
+				                 << event.peer->address.port);
+
+				CreatePeer( *event.peer );
+				break;
+			}
+			case ENET_EVENT_TYPE_RECEIVE: {
+				auto peer = extractPeer();
+				DEBUG::LOG_ENTRY( std::stringstream() << "Packet received." << std::endl
+				                  << "\tLength: " << event.packet->dataLength
+				                  << "\tFrom: " << event.peer->address.host << ":" << event.peer->address.port);
+
+				peer->Receive(*event.packet);
+
+				// Clean up the packet now that we're done using it.
+				enet_packet_destroy(event.packet);
+				break;
+			}
+			case ENET_EVENT_TYPE_DISCONNECT: {
+				auto peer = extractPeer();
+				event.peer->data = nullptr;
+
+				std::cout << "A client disconnected: " << peer->address.host
+						  << ":" << peer->address.port
+						  << std::endl;
+
+				// enet code did clean itself up
+				peer->enetPeer = nullptr;
+
+				// We set the user data of Event to null so
+				// we are allowed to remove the shared_ptr
+				_peers.connected.erase(peer->address);
+				_peers.unconnected.insert(peer->address);
+				_disconnectCallback(*peer);
+				break;
+			}
+		}
+	}
+
+	//
+	// Do output handling
+	//
+
+	// Exchange public with private queue so other
+	// threads may fill the external queue without
+	// need to do complicated locking
+	_atomicQueues[0].exchange(_atomicQueues[1]);
+
+	// We now can process the new private queue
+	// since it is no longer used by another thread
+
+	auto& currentQueue = *_atomicQueues[1].load();
+	//TODO: introduce locking
+	// Process everything we have in the queue
+	for( auto& element : currentQueue ) {
+		if( element.peer )
+			// Send to peer
+			element.peer->Send( element.buffer, element.callback );
+		else
+			// Do broadcast
+			Send( element.buffer, element.callback );
+	}
+
+	return true;
+}
+
+void Network::ExecuteWorker() {
+	static const std::chrono::milliseconds duration( 1 );
+
+	DEBUG::LOG_ENTRY("[Worker] Start...");
+
+	while( _ExecuteWorker() ) {
+		auto queue = _atomicQueues[0].load();
+		if( queue == nullptr )
+			// Should never happen but paranoia is so sweet
+			continue;
+
+		if( queue->empty() )
+			// If there is nothing to do - do not spam
+			// the CPU
+			std::this_thread::sleep_for( duration );
+	}
+
+	DEBUG::LOG_ENTRY("[Worker] ...End");
+}
+
+void Network::ConnectTo( const std::string& host, enet_uint16 port ) {
+	ENetAddress address;
+
+	// Connect to other peer
+	if( enet_address_set_host( &address, host.c_str() ) != 0 ) {
+		throw std::runtime_error("Could no set address");
+	}
+	address.port = port;
+
+	// Make sure we are the only thread accessing object
+	std::lock_guard<std::mutex> lock( _peers.unconnectedLock );
+	_peers.unconnected.insert( address );
+}
---
+++ src/net/network.hpp
@@ -0,0 +1,151 @@
+/*
+ * network.hpp
+ *
+ *  Created on: Jun 15, 2012
+ *      Author: andreas
+ */
+
+#ifndef NETWORK_HPP_
+#define NETWORK_HPP_
+
+#include <atomic>
+#include <memory>
+#include <map>
+#include <array>
+#include <deque>
+#include <queue>
+#include <functional>
+#include <tuple>
+#include <set>
+#include <istream>
+#include <ostream>
+#include <mutex>
+//#include <Poco/Net/SocketAddress.h>
+#include <enet/enet.h>
+#include <future>
+#include "../subsystem.hpp"
+
+extern bool operator<(const ENetAddress& lhs, const ENetAddress& rhs);
+
+namespace hack {
+namespace net {
+
+class Network : public hack::Subsystem {
+public:
+	typedef std::vector<enet_uint8> buffer_type;
+
+	Network( enet_uint16 incomingPort );
+	class Peer {
+		// Make sure we can only be created by the network
+		friend class Network;
+		Peer(ENetAddress address);
+		void Destroy();
+		std::queue<std::tuple<buffer_type, std::function<void()>>> sendQueue;
+		std::string _uuid;
+		ENetPeer* enetPeer = nullptr;
+		ENetPeer* GetPeer();
+		void Disconnect();
+		void Receive(const ENetPacket& packet);
+		void SetUUID(std::string uuid);
+	public:
+		~Peer();
+		bool operator < (const Peer& other) const;
+		bool operator ==(const Peer& other) const;
+		std::function<void(buffer_type)> receiveCallback;
+		void Send(buffer_type buffer, std::function<void()> callback);
+		const std::string& GetUUID() const;
+
+		const ENetAddress address;
+	};
+
+private:
+	std::function<void(std::shared_ptr<Peer>)> _connectCallback;
+	std::function<void(hack::net::Network::Peer&)> _disconnectCallback;
+	void Destroy();
+	void CreatePeer( ENetPeer& event );
+
+	struct DEBUG {
+		static std::ostream& LOG      ( std::ostream& stream );
+		static std::ostream& LOG      ( const std::string& str );
+		static std::ostream& ERR      ( std::ostream& stream );
+		static void         LOG_ENTRY( std::ostream& stream);
+		static void         LOG_ENTRY( const std::string& str);
+		static void         ERR_ENTRY( std::ostream& stream);
+	};
+
+	struct queue_element_type {
+		// Destination of data - broadcast if peer is null
+		std::shared_ptr<Peer> peer;
+		buffer_type buffer;
+		 std::function<void()> callback;
+	};
+
+	std::array<std::unique_ptr<std::deque<queue_element_type>>, 2> _queues;
+	std::array<std::atomic<std::deque<queue_element_type>*>, 2> _atomicQueues;
+
+
+
+	struct {
+		// Mutex to be used when object is accessed
+		std::mutex unconnectedLock;
+		// All known addresses of other peers
+		std::set<ENetAddress> unconnected;
+		// All peers which are connected
+		std::map<ENetAddress, std::shared_ptr<Peer>> connected;
+	} _peers;
+
+	ENetHost* _client, * _server;
+
+	template <typename T>
+	static ENetPacket* _createPacket(const T& buffer) {
+		const size_t byteCount = buffer.size() * sizeof(typename T::value_type);
+		auto packet = enet_packet_create( buffer.data(),
+		                                  byteCount,
+		                                  ENetPacketFlag::ENET_PACKET_FLAG_RELIABLE
+		);
+
+		if( packet == nullptr )
+			throw std::runtime_error("Could not create packet!");
+		return packet;
+	}
+
+public:
+	virtual ~Network() noexcept;
+	void Setup();
+
+	// Processes the queue indefinitely
+	void ExecuteWorker();
+
+	template <typename T>
+	void Send(const T& buffer, std::function<void()> callback) {
+		auto packet = _createPacket(buffer);
+		enet_host_broadcast(_client, 0, packet);
+		std::async(std::launch::async, callback);
+	}
+
+	template <typename T>
+	void Send(const T& buffer) {
+		Send(buffer, []() {});
+	}
+
+
+	// Callback is executed synchronously. Make sure it returns very fast to not
+	// block the network communication!
+	void SetReceiveCallback(std::function<void(buffer_type)> callback);
+	void SetConnectCallback(std::function<void(std::shared_ptr<hack::net::Network::Peer>)> callback);
+	void SetDisconnectCallback(std::function<void(hack::net::Network::Peer&)> callback);
+	void ConnectTo( const std::string& host, enet_uint16 port );
+private:
+
+	// Processes a filled queue
+	bool _ExecuteWorker();
+	// Process all known peers that are not yet connected
+	void HandleUnconnected();
+
+
+};
+
+} }
+
+
+#endif /* NETWORK_HPP_ */
