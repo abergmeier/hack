d4c84ab5ea7cf144a88109dbe295cae724987b27
Rykers <improved.cut.red.tape@googlemail.com>
2012-06-26 15:22:54 +0200

    UUID fuer Windows

--- src/state/local_player.cpp
+++ src/state/local_player.cpp
@@ -7,24 +7,42 @@
 
 #include <functional>
 #include <queue>
-#include <Poco/Base64Encoder.h>
+//#include <Poco/Base64Encoder.h>
 #include <sstream>
-#include <uuid/uuid.h>
+#ifdef _MSC_VER
+	#include <rpc.h>
+#else
+	#include <uuid/uuid.h>
+#endif // DEBUG
 #include "local_player.hpp"
 #include "../include.hpp"
 #include "../logic/states.hpp"
 
+
 namespace {
 	std::string generateUUID() {
+
+		
+#ifdef _MSC_VER
+		UUID id;
+		RPC_CSTR cstr;
+		UuidCreate(&id);
+		UuidToStringA(&id,&cstr);
+		std::string uuid(reinterpret_cast<char*>(cstr));
+		RpcStringFreeA(&cstr);
+#else
 		// We have to use array here because
 		// std::string does not provide access to
 		// internal char array
 		std::array<char, 16> uuidBuffer;
-
+		
 		// Generate function wants buffer unsigned but string does only accept signed.
 		// Since sizes are the same - cast to unsigned for function.
 		uuid_generate_random(reinterpret_cast<unsigned char*>(uuidBuffer.data()));
 		std::string uuid(uuidBuffer.data(), uuidBuffer.size());
+#endif // _MSC_VER
+
+
 		return std::move(uuid);
 	}
 }
