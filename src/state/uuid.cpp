82bf1f3e919f8f1b763b3055b927618519cef7a4
LCID Fire <lcid-fire@gmx.net>
2012-06-30 22:40:09 +0200

    Replace platform specific UUID generation with POCO implementation.

--- src/state/uuid.cpp
+++ src/state/uuid.cpp
@@ -5,42 +5,16 @@
  *      Author: andreas
  */
 
-#ifdef noexcept
-#undef noexcept
-#endif //noexcept
-
-#ifdef _MSC_VER
-	#define _WINSOCKAPI_
-	#include <windows.h>
-	#include <Rpc.h>
-#else
-	#include <array>
-	#include <uuid/uuid.h>
-#endif // DEBUG
+#include <Poco/UUIDGenerator.h>
+#include <Poco/UUID.h>
 #include "uuid.hpp"
 
 using namespace hack;
 
 std::string hack::UUID::Generate() {
-#ifdef _MSC_VER
-		::UUID id;
-		RPC_CSTR cstr;
-		UuidCreate(&id);
-		UuidToStringA(&id,&cstr);
-		std::string uuid(reinterpret_cast<char*>(cstr));
-		RpcStringFreeA(&cstr);
-#else
-		// We have to use array here because
-		// std::string does not provide access to
-		// internal char array
-		std::array<char, 16> uuidBuffer;
-
-		// Generate function wants buffer unsigned but string does only accept signed.
-		// Since sizes are the same - cast to unsigned for function.
-		uuid_generate_random(reinterpret_cast<unsigned char*>(uuidBuffer.data()));
-		std::string uuid(uuidBuffer.data(), uuidBuffer.size());
-#endif // _MSC_VER
-		return uuid;
+	Poco::UUIDGenerator& generator = Poco::UUIDGenerator::defaultGenerator();
+	Poco::UUID uuid(generator.createRandom());
+	return uuid.toString();
 }
 
 
