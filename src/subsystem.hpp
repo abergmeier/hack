31883d9f342e1a0128ce93916f3d6c438fbb93f4
LCID Fire <lcid-fire@gmx.net>
2012-07-03 00:53:53 +0200

    Provide destructorMutex, which is needed to be lockable before destruction of object.

--- src/subsystem.hpp
+++ src/subsystem.hpp
@@ -8,17 +8,20 @@
 #ifndef SUBSYSTEM_HPP_
 #define SUBSYSTEM_HPP_
 
+#include <mutex>
+
 namespace hack {
 
 // Class to be implemented by any Subsystem that has a clear and limited
 // architecture for communication.
 class Subsystem {
 public:
-	virtual ~Subsystem();
+	virtual ~Subsystem(){ std::lock_guard<std::mutex> lock( destructorMutex ); };
 	// Start the worker loop. Should always be execute in a seperate process.
 	virtual void ExecuteWorker() = 0;
 protected:
-	Subsystem();
+	Subsystem(){};
+	std::mutex destructorMutex;
 };
 
 }
